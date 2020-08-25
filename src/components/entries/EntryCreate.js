a0f052746120b20ad5eb4c226ccbf5d8dd530305
Maja Zubko <majamzubko@gmail.com>
2020-08-25 17:42:56 +0200

    added logic of adding new entry

--- src/components/entries/EntryCreate.js
+++ src/components/entries/EntryCreate.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const EntryCreate = () => {
-	return <div>EntryCreate</div>;
+class EntryCreate extends React.Component {
+	renderError({ error, touched }) {
+		if (touched && error) {
+			return (
+				<div className="error-field">
+					<div className="error-header">{error}</div>
+				</div>
+			);
+		}
+	}
+
+	renderInput = ({ input, label, type, step, meta }) => {
+		const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+
+		return (
+			<div className={className}>
+				<label>{label}</label>
+				<input {...input} type={type} step={step} autoComplete="off" />
+				{this.renderError(meta)}
+			</div>
+		);
+	};
+
+	onSubmit(formValues) {}
+
+	render() {
+		return (
+			<form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+				<Field name="date" component={this.renderInput} label="Enter date" type="date" />
+				<Field name="temperature" component={this.renderInput} label="Enter temperature" type="number" step=".01" />
+				<button className="submit-button">Submit</button>
+			</form>
+		);
+	}
+}
+
+const validate = (formValues) => {
+	const errors = {};
+
+	if (!formValues.date) {
+		errors.date = 'You must enter a date';
+	}
+
+	if (!formValues.temperature) {
+		errors.temperature = 'You must enter a temperature';
+	}
+
+	return errors;
 };
 
-export default EntryCreate;
+export default reduxForm({
+	form: 'entryCreate',
+	validate
+})(EntryCreate);
